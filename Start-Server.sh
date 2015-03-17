22d39ea376caa9c6d525812bd4a729c54e6c3eae
Waishon <soeren.2011@live.de>
2015-03-17 17:12:29 +0100

    Server Start Bash hinzugefügt

---
+++ Start-Server.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Diese Datei sorgt dafür, dass der Server neugestartet wird,
+# sobald dieser crashen sollte -> Notaus etc.
+
+# Dauerschleife
+while true;
+do
+# THOMAS-Server Software aufrufen
+output=$("./thomas")
+
+# Fehler in Syslog schreiben
+logger "[THOMAS] Programm abgestürzt - Fehler:"
+logger "[THOMAS] "$output
+
+# Fehler ausgeben
+echo $output
+
+# 10 Sekunden warten
+sleep 10
+done
