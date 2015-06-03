c1e24af4ceaa47ed2dafccfc5e215f76b49ed0dc
Marcus Wichelmann <marcus.wichelmann@hotmail.de>
2015-06-03 14:02:25 +0200

    Ungültige Sensorwerte ignorieren

--- CollisionDetection.cpp
+++ CollisionDetection.cpp
@@ -52,7 +52,7 @@ std::vector<short> CollisionDetection::CorrectWantedSpeed(short wantedSpeed[])
 	std::vector<short> newWantedSpeed (2);
 
 	// Ist vorne frei?
-	if(USensorMessurements.at(US_FRONT_MIDDLE) > _warnDistance)
+	if(USensorMessurements.at(US_FRONT_MIDDLE) > _warnDistance || USensorMessurements.at(US_FRONT_MIDDLE) == 0)
 	{
 		// Geradeaus weiter fahren
 		newWantedSpeed.at(0) = wantedSpeed[0];
@@ -62,7 +62,7 @@ std::vector<short> CollisionDetection::CorrectWantedSpeed(short wantedSpeed[])
 	}
 
 	// Ist Links frei
-	if(USensorMessurements.at(US_FRONT_LEFT) > _warnDistanceRL)
+	if(USensorMessurements.at(US_FRONT_LEFT) > _warnDistanceRL || USensorMessurements.at(US_FRONT_LEFT) == 0)
 	{
 		// Nach links fahren
 		newWantedSpeed.at(0) = 0;
@@ -72,7 +72,7 @@ std::vector<short> CollisionDetection::CorrectWantedSpeed(short wantedSpeed[])
 	}
 
 	// Ist Rechts frei
-	if(USensorMessurements.at(US_FRONT_RIGHT) > _warnDistanceRL)
+	if(USensorMessurements.at(US_FRONT_RIGHT) > _warnDistanceRL || USensorMessurements.at(US_FRONT_RIGHT) == 0)
 	{
 		// Nach rechts fahren
 		newWantedSpeed.at(0) = 32767;
@@ -87,7 +87,7 @@ std::vector<short> CollisionDetection::CorrectWantedSpeed(short wantedSpeed[])
 	newWantedSpeed.at(1) = 0;
 
 	// OsterEi: THOMAS Dead Sound -> Wer hat da eine Tür hingestellt?
-	system("beep -f 523 -l 38 -D 2 -n -f 554 -l 34 -D 2 -n -f 587 -l 28 -D 489 -n -f 195 -l 20 -D 0 -n -f 391 -l 20 -D 0 -n -f 493 -l 20 -n -f 195 -l 20 -D 0 -n -f 391 -l 20 -D 0 -n -f 493 -l 20 -D 0 -n -f 587 -l 20 -D 0 -n -f 698 -l 20 -n -f 587 -l 20 -D 0 -n -f 698 -l 20 -n -f 587 -l 20 -D 0 -n -f 698 -l 20 -D 145 -n -f 195 -l 20 -D 0 -n -f 587 -l 20 -D 0 -n -f 698 -l 20 -n -f 195 -l 20 -D 0 -n -f 587 -l 20 -D 0 -n -f 698 -l 20 -D 0 -n -f 195 -l 20 -D 0 -n -f 587 -l 20 -D 0 -n -f 698 -l 20 -D 99 -n -f 220 -l 20 -D 0 -n -f 523 -l 20 -D 0 -n -f 659 -l 20 -D 99 -n -f 246 -l 20 -D 0 -n -f 493 -l 20 -D 0 -n -f 587 -l 20 -D 99 -n -f 261 -l 20 -D 0 -n -f 391 -l 20 -D 0 -n -f 523 -l 20 -n -f 261 -l 20 -D 0 -n -f 391 -l 20 -D 0 -n -f 523 -l 20 -D 0 -n -f 329 -l 148 -D 0 -n -f 195 -l 147 -D 0 -n -f 329 -l 147 -D 0 -n -f 130 -l 20 -D 0 -n -f 261 -l 20 -n -f 130 -l 20 -D 0 -n -f 261 -l 20 -n -f 130 -l 20 -D 0 -n -f 261 -l 20 &");
+	//system("beep -f 523 -l 38 -D 2 -n -f 554 -l 34 -D 2 -n -f 587 -l 28 -D 489 -n -f 195 -l 20 -D 0 -n -f 391 -l 20 -D 0 -n -f 493 -l 20 -n -f 195 -l 20 -D 0 -n -f 391 -l 20 -D 0 -n -f 493 -l 20 -D 0 -n -f 587 -l 20 -D 0 -n -f 698 -l 20 -n -f 587 -l 20 -D 0 -n -f 698 -l 20 -n -f 587 -l 20 -D 0 -n -f 698 -l 20 -D 145 -n -f 195 -l 20 -D 0 -n -f 587 -l 20 -D 0 -n -f 698 -l 20 -n -f 195 -l 20 -D 0 -n -f 587 -l 20 -D 0 -n -f 698 -l 20 -D 0 -n -f 195 -l 20 -D 0 -n -f 587 -l 20 -D 0 -n -f 698 -l 20 -D 99 -n -f 220 -l 20 -D 0 -n -f 523 -l 20 -D 0 -n -f 659 -l 20 -D 99 -n -f 246 -l 20 -D 0 -n -f 493 -l 20 -D 0 -n -f 587 -l 20 -D 99 -n -f 261 -l 20 -D 0 -n -f 391 -l 20 -D 0 -n -f 523 -l 20 -n -f 261 -l 20 -D 0 -n -f 391 -l 20 -D 0 -n -f 523 -l 20 -D 0 -n -f 329 -l 148 -D 0 -n -f 195 -l 147 -D 0 -n -f 329 -l 147 -D 0 -n -f 130 -l 20 -D 0 -n -f 261 -l 20 -n -f 130 -l 20 -D 0 -n -f 261 -l 20 -n -f 130 -l 20 -D 0 -n -f 261 -l 20 &");
 
 	return newWantedSpeed;
 
